9db7a05d4ea7f1170bd9924c7d3626aca92cb26c
Daniel Reed <nmlorg@gmail.com>
2022-04-15 14:30:49 -0700

    Use a more visually accessible tree representation.

--- estree_test.js
+++ estree_test.js
@@ -11,42 +11,29 @@ export function test_tokenize(U) {
 
 function* flattenTree(node) {
   if (Array.isArray(node)) {
-    if (!node.length) {
-      yield '[]';
-      return;
+    yield 'Array';
+    for (let i = 0; i < node.length; i++) {
+      let last = i == node.length - 1;
+      let lines = Array.from(flattenTree(node[i]));
+      yield `  ${last ? '\u2514' : '\u251c'}\u2192 ${lines[0]}`;
+      for (let j = 1; j < lines.length; j++)
+        yield `  ${last ? ' ' : '\u2502'}  ${lines[j]}`;
     }
-    yield '[';
-    for (let v of node) {
-      let lines = Array.from(flattenTree(v));
-      if (lines.length == 1) {
-        yield `    ${lines[0]},`;
-        continue;
-      }
-      for (let j = 0; j < lines.length - 1; j++)
-        yield `    ${lines[j]}`;
-      yield `    ${lines[lines.length - 1]},`;
-    }
-    yield ']';
     return;
   }
   if (!(node instanceof estree.Node)) {
     yield JSON.stringify(node);
     return;
   }
-  yield `${node.constructor.name}(`;
+  yield `${node.constructor.name}`;
   let children = Object.entries(node);
   for (let i = 0; i < children.length; i++) {
-    let ending = i < children.length - 1 ? ',' : ')';
+    let last = i == children.length - 1;
     let [k, v] = children[i];
     let lines = Array.from(flattenTree(v));
-    if (lines.length == 1) {
-      yield `    ${k}=${lines[0]}${ending}`;
-      continue;
-    }
-    yield `    ${k}=${lines[0]}`;
-    for (let j = 1; j < lines.length - 1; j++)
-      yield `    ${lines[j]}`;
-    yield `    ${lines[lines.length - 1]}${ending}`;
+    yield `  ${last ? '\u2514' : '\u251c'}\u2192 ${k}: ${lines[0]}`;
+    for (let j = 1; j < lines.length; j++)
+      yield `  ${last ? ' ' : '\u2502'}  ${lines[j]}`;
   }
 }
 
@@ -56,99 +43,97 @@ export function test_buildTree(U) {
   let tokens = estree.tokenize(expr);
   let tree = estree.buildTree(tokens);
   U.assert(Array.from(flattenTree(tree)).join('\n') == `\
-Identifier(
-    name="aa")`);
+Identifier
+  └→ name: "aa"`);
 
   expr = 'aa == bb';
   tokens = estree.tokenize(expr);
   tree = estree.buildTree(tokens);
   U.assert(Array.from(flattenTree(tree)).join('\n') == `\
-BinaryExpression(
-    left=Identifier(
-        name="aa"),
-    operator="==",
-    right=Identifier(
-        name="bb"))`);
+BinaryExpression
+  ├→ left: Identifier
+  │    └→ name: "aa"
+  ├→ operator: "=="
+  └→ right: Identifier
+       └→ name: "bb"`);
 
   expr = 'aa.bb';
   tokens = estree.tokenize(expr);
   tree = estree.buildTree(tokens);
   U.assert(Array.from(flattenTree(tree)).join('\n') == `\
-MemberExpression(
-    object=Identifier(
-        name="aa"),
-    property=Literal(
-        value="bb",
-        raw="bb"))`);
+MemberExpression
+  ├→ object: Identifier
+  │    └→ name: "aa"
+  └→ property: Literal
+       ├→ value: "bb"
+       └→ raw: "bb"`);
 
   expr = 'aa.bb.cc';
   tokens = estree.tokenize(expr);
   tree = estree.buildTree(tokens);
   U.assert(Array.from(flattenTree(tree)).join('\n') == `\
-MemberExpression(
-    object=MemberExpression(
-        object=Identifier(
-            name="aa"),
-        property=Literal(
-            value="bb",
-            raw="bb")),
-    property=Literal(
-        value="cc",
-        raw="cc"))`);
+MemberExpression
+  ├→ object: MemberExpression
+  │    ├→ object: Identifier
+  │    │    └→ name: "aa"
+  │    └→ property: Literal
+  │         ├→ value: "bb"
+  │         └→ raw: "bb"
+  └→ property: Literal
+       ├→ value: "cc"
+       └→ raw: "cc"`);
 
   expr = 'aa()';
   tokens = estree.tokenize(expr);
   tree = estree.buildTree(tokens);
   U.assert(Array.from(flattenTree(tree)).join('\n') == `\
-CallExpression(
-    callee=Identifier(
-        name="aa"),
-    arguments=[])`);
+CallExpression
+  ├→ callee: Identifier
+  │    └→ name: "aa"
+  └→ arguments: Array`);
 
   expr = 'aa(bb, cc)';
   tokens = estree.tokenize(expr);
   tree = estree.buildTree(tokens);
   U.assert(Array.from(flattenTree(tree)).join('\n') == `\
-CallExpression(
-    callee=Identifier(
-        name="aa"),
-    arguments=[
-        Identifier(
-            name="bb"),
-        Identifier(
-            name="cc"),
-    ])`);
+CallExpression
+  ├→ callee: Identifier
+  │    └→ name: "aa"
+  └→ arguments: Array
+       ├→ Identifier
+       │    └→ name: "bb"
+       └→ Identifier
+            └→ name: "cc"`);
 
   expr = "board.get('a1', foo()).piece.moves == 123";
   tokens = estree.tokenize(expr);
   tree = estree.buildTree(tokens);
   U.assert(Array.from(flattenTree(tree)).join('\n') == `\
-BinaryExpression(
-    left=MemberExpression(
-        object=MemberExpression(
-            object=CallExpression(
-                callee=MemberExpression(
-                    object=Identifier(
-                        name="board"),
-                    property=Literal(
-                        value="get",
-                        raw="get")),
-                arguments=[
-                    Identifier(
-                        name="'a1'"),
-                    CallExpression(
-                        callee=Identifier(
-                            name="foo"),
-                        arguments=[]),
-                ]),
-            property=Literal(
-                value="piece",
-                raw="piece")),
-        property=Literal(
-            value="moves",
-            raw="moves")),
-    operator="==",
-    right=Literal(
-        value=123,
-        raw="123"))`);
+BinaryExpression
+  ├→ left: MemberExpression
+  │    ├→ object: MemberExpression
+  │    │    ├→ object: CallExpression
+  │    │    │    ├→ callee: MemberExpression
+  │    │    │    │    ├→ object: Identifier
+  │    │    │    │    │    └→ name: "board"
+  │    │    │    │    └→ property: Literal
+  │    │    │    │         ├→ value: "get"
+  │    │    │    │         └→ raw: "get"
+  │    │    │    └→ arguments: Array
+  │    │    │         ├→ Identifier
+  │    │    │         │    └→ name: "'a1'"
+  │    │    │         └→ CallExpression
+  │    │    │              ├→ callee: Identifier
+  │    │    │              │    └→ name: "foo"
+  │    │    │              └→ arguments: Array
+  │    │    └→ property: Literal
+  │    │         ├→ value: "piece"
+  │    │         └→ raw: "piece"
+  │    └→ property: Literal
+  │         ├→ value: "moves"
+  │         └→ raw: "moves"
+  ├→ operator: "=="
+  └→ right: Literal
+       ├→ value: 123
+       └→ raw: "123"`);
 }
